b859c280aa2d795e5b262eff595197811e897f73
Jan Ramsak <johnyhiter@gmail.com>
2025-01-12 19:42:42 +0100

    Commit 3

--- angleska-igra.py
+++ angleska-igra.py
@@ -4,7 +4,11 @@ pot1 = "ang_besede.txt"
 pot2 = "slo_besede.txt"
 tocke = 0
 ponovi = 0
+izbrano = 0
 ponovi_besede = []
+ponovljeno = []
+stevilo_angleskih_besed = 119
+stevilo_sprasevanj = 10 # Ne more jih biti več kot stevilo_angleshih_besed
 print("Angleška igra: ")
 print("Pravila igre: ")
 print("Ta igra te testira koliko angleških besed poznaš.\nTi moraš samo povedati pravilno besedo.")
@@ -14,8 +18,11 @@ with open(pot1, 'r', encoding='utf-8') as file:
 with open(pot2, 'r', encoding='utf-8') as file:
     slo_besede = [line.strip() for line in file]
 
-for i in range (1, 11):
-    izbrano = random.randint(0, 9)
+for i in range (1, stevilo_sprasevanj+1):
+    izbrano = random.randint(0, stevilo_angleskih_besed)
+    while izbrano in ponovljeno:
+        izbrano = random.randint(0, 9)
+    ponovljeno.append(izbrano)
     beseda_rac = ang_besede[izbrano]
     beseda = input("Vnesi slovensko besedo za besedo " + str(beseda_rac) + ": ")
     if beseda == slo_besede[izbrano]:

f64b5fd4854f7259c2b75fc03b9f937a1ce318a4
Jan Ramsak <johnyhiter@gmail.com>
2025-01-12 19:22:47 +0100

    Commit 2

--- angleska-igra.py
+++ angleska-igra.py
@@ -3,6 +3,8 @@ import random
 pot1 = "ang_besede.txt"
 pot2 = "slo_besede.txt"
 tocke = 0
+ponovi = 0
+ponovi_besede = []
 print("Angleška igra: ")
 print("Pravila igre: ")
 print("Ta igra te testira koliko angleških besed poznaš.\nTi moraš samo povedati pravilno besedo.")
@@ -15,9 +17,16 @@ with open(pot2, 'r', encoding='utf-8') as file:
 for i in range (1, 11):
     izbrano = random.randint(0, 9)
     beseda_rac = ang_besede[izbrano]
-    beseda = input("Vnesi slovensko besedo za besede " + str(beseda_rac) + ": ")
+    beseda = input("Vnesi slovensko besedo za besedo " + str(beseda_rac) + ": ")
     if beseda == slo_besede[izbrano]:
         tocke += 1
         print("Napisal si pravilno besedo. Točke: "+str(tocke)+"/"+str(i))
     else:
+        ponovi += 1
+        ponovi_besede.append(beseda_rac)
         print("Beseda se ne ujema. Točke: "+str(tocke)+"/"+str(i))
+print("Tukaj so besede, ki jih nisi vedel: ")
+print(", ".join(ponovi_besede))
+print("-------------------------------------------------------------------")
+print("Dosežene točke:", tocke)
+print("Dosežen odstotek: "+str(int((tocke/10)*100))+"%")

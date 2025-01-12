1ccdcfc1016db72cb73a176abd3dd72bc05be30f
Jan Ramsak <johnyhiter@gmail.com>
2025-01-12 22:59:37 +0100

    Commit 4

--- angleska-igra.py
+++ angleska-igra.py
@@ -1,5 +1,8 @@
 import random
 
+rdeca = '\033[31m'
+reset = '\033[0m'
+zelena = '\033[32m'
 pot1 = "ang_besede.txt"
 pot2 = "slo_besede.txt"
 tocke = 0
@@ -19,21 +22,21 @@ with open(pot2, 'r', encoding='utf-8') as file:
     slo_besede = [line.strip() for line in file]
 
 for i in range (1, stevilo_sprasevanj+1):
-    izbrano = random.randint(0, stevilo_angleskih_besed)
+    izbrano = random.randint(0, stevilo_angleskih_besed-1)
     while izbrano in ponovljeno:
         izbrano = random.randint(0, 9)
     ponovljeno.append(izbrano)
     beseda_rac = ang_besede[izbrano]
-    beseda = input("Vnesi slovensko besedo za besedo " + str(beseda_rac) + ": ")
+    beseda = input("Vnesi slovensko besedo za besedo " + zelena + str(beseda_rac) + reset + ": ")
     if beseda == slo_besede[izbrano]:
         tocke += 1
-        print("Napisal si pravilno besedo. Točke: "+str(tocke)+"/"+str(i))
+        print(zelena + "Napisal si pravilno besedo. " + reset + "Točke: "+str(tocke)+"/"+str(i))
     else:
         ponovi += 1
         ponovi_besede.append(beseda_rac)
-        print("Beseda se ne ujema. Točke: "+str(tocke)+"/"+str(i))
+        print(rdeca + "Beseda se ne ujema. Točke: " + reset +str(tocke)+"/"+str(i))
 print("Tukaj so besede, ki jih nisi vedel: ")
-print(", ".join(ponovi_besede))
-print("-------------------------------------------------------------------")
+print(rdeca + ", ".join(ponovi_besede) + reset)
+print(zelena + "-------------------------------------------------------------------")
 print("Dosežene točke:", tocke)
-print("Dosežen odstotek: "+str(int((tocke/10)*100))+"%")
+print("Dosežen odstotek: "+str(int((tocke/10)*100))+"%" + reset)
